8efc76c45dc160f0a31305146b1374a7f0c97233
Micah Worth <mjw324@lehigh.edu>
2023-04-10 15:26:13 -0400

    Successfully connected mySQL db with google oath

---
+++ server/app.js
@@ -0,0 +1,40 @@
+// imports environment variables, which are stored in .env file.
+require('dotenv').config();
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+// var cookieParser = require('cookie-parser');
+var session = require('express-session');
+// var csrf = require('csurf');
+var passport = require('passport');
+// var logger = require('morgan');
+
+// Import MySQL session store
+// var MySQLStore = require('express-mysql-session')(session);
+// var database = require('./boot/database'); // Import the MySQL pool
+var indexRouter = require('./routes/index');
+var authRouter = require('./routes/auth');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// app.locals.pluralize = require('pluralize');
+
+// app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+app.use('/', indexRouter);
+app.use('/', authRouter);
