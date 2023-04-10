a62e49492b33ede5c0be9cbdc46d79c217a43c9d
Micah Worth <mjw324@lehigh.edu>
2023-04-10 15:26:13 -0400

    Successfully connected mySQL db with google oath

---
+++ server/boot/database.js
@@ -0,0 +1,55 @@
+var mysql = require('mysql2');
+const mkdirp = require('mkdirp');
+
+mkdirp.sync('./var/db');
+
+
+// Database configuration
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+};
+
+// Creating a MySQL pool - https://www.npmjs.com/package/mysql#pooling-connections
+// According to Wikipedia, 
+// "connection pool is a cache of database connections maintained so that the 
+// connections can be reused when future requests to the database are required"
+// This avoids creating a connection to the database each time it needs accessed by user logging in/configuriing their monitor
+var db_pool = mysql.createPool(dbConfig);
+
+
+db_pool.query(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE,
+    hashed_password VARBINARY(255),
+    salt VARBINARY(255),
+    name VARCHAR(255)
+  )
+`);
+
+db_pool.query(`
+  CREATE TABLE IF NOT EXISTS federated_credentials (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    provider VARCHAR(255) NOT NULL,
+    subject VARCHAR(255) NOT NULL,
+    UNIQUE (provider, subject)
+  )
+`);
+
+// Creating monitors table, holding the search parameters for each monitor and the respective owner
+db_pool.query(`
+  CREATE TABLE IF NOT EXISTS monitors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    owner_id INT NOT NULL,
+    search_parameters VARCHAR(255) NOT NULL,
+  )
+`);
+
+module.exports = db_pool;
---
+++ server/routes/auth.js
@@ -0,0 +1,117 @@
+var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oidc');
+var db = require('../db'); // mysql database
+
+
+// Configure the Google strategy for use by Passport.
+//
+// OAuth 2.0-based strategies require a `verify` function which receives the
+// credential (`accessToken`) for accessing the Google API on the user's
+// behalf, along with the user's profile.  The function must invoke `cb`
+// with a user object, which will be set at `req.user` in route handlers after
+// authentication.
+passport.use(new GoogleStrategy({
+  clientID: process.env['GOOGLE_CLIENT_ID'],
+  clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
+  callbackURL: '/oauth2/redirect/google',
+  scope: [ 'profile' ]
+}, function verify(issuer, profile, cb) {
+  db.pool.query('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [issuer, profile.id], (error, results, fields) => {
+      if (error) { return cb(error); }
+      if (results.length === 0) {
+        db.pool.query('INSERT INTO users (name) VALUES (?)', [profile.displayName], (error, results, fields) => {
+          if (error) { return cb(error); }
+          var id = results.insertId;
+          db.pool.query('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [id, issuer, profile.id], (error, results, fields) => {
+            if (error) { return cb(error); }
+            var user = {
+              id: id,
+              name: profile.displayName
+            };
+            return cb(null, user);
+          });
+        });
+      } else {
+        db.pool.query('SELECT * FROM users WHERE id = ?', [ results[0].user_id ], (error, results, fields) => {
+          if (error) { return cb(error); }
+          if (results.length === 0) { return cb(null, false); }
+          return cb(null, results[0]);
+        });
+      }
+    });
+}));
+  
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  In a
+// production-quality application, this would typically be as simple as
+// supplying the user ID when serializing, and querying the user record by ID
+// from the database when deserializing.  However, due to the fact that this
+// example does not have a database, the complete Facebook profile is serialized
+// and deserialized.
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    cb(null, { id: user.id, username: user.username, name: user.name });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+
+var router = express.Router();
+
+/* GET /login
+ *
+ * This route prompts the user to log in.
+ *
+ * The 'login' view renders an HTML page, which contain a button prompting the
+ * user to sign in with Google.  When the user clicks this button, a request
+ * will be sent to the `GET /login/federated/accounts.google.com` route.
+ */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+/* GET /login/federated/accounts.google.com
+ *
+ * This route redirects the user to Google, where they will authenticate.
+ *
+ * Signing in with Google is implemented using OAuth 2.0.  This route initiates
+ * an OAuth 2.0 flow by redirecting the user to Google's identity server at
+ * 'https://accounts.google.com'.  Once there, Google will authenticate the user
+ * and obtain their consent to release identity information to this app.
+ *
+ * Once Google has completed their interaction with the user, the user will be
+ * redirected back to the app at `GET /oauth2/redirect/accounts.google.com`.
+ */
+router.get('/login/federated/google', passport.authenticate('google'));
+
+/*
+    This route completes the authentication sequence when Google redirects the
+    user back to the application.  When a new user signs in, a user account is
+    automatically created and their Google account is linked.  When an existing
+    user returns, they are signed in to their linked account.
+*/
+router.get('/oauth2/redirect/google', passport.authenticate('google', {
+  successReturnToOrRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+/* POST /logout
+ *
+ * This route logs the user out.
+ */
+router.post('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+module.exports = router;
