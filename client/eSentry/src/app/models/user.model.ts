cfeefec7cfe99ce1eeb6863151f39a8829a287c0
Paolo Bartolucci <paolobarto210@gmail.com>
2023-05-12 14:57:58 -0400

    added id

--- client/eSentry/src/app/models/user.model.ts
+++ client/eSentry/src/app/models/user.model.ts
@@ -2,10 +2,12 @@ export type User = {
     photoUrl: string;
     name: string;
     email: string;
+    id: string;
 };
 
 export const DEFAULT_USER: User = {
     photoUrl:"",
     name:"",
-    email:""
+    email:"",
+    id:""
 };
