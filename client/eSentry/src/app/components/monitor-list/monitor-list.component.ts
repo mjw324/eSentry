9cc193fd2cc77723078c0cc1295c1371866770c2
Paolo Bartolucci <paolobarto210@gmail.com>
2023-05-14 11:58:10 -0400

    updated dialog

--- client/eSentry/src/app/components/monitor-list/monitor-list.component.ts
+++ client/eSentry/src/app/components/monitor-list/monitor-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Monitor } from 'src/app/models/monitor.model';
 import { MonitorService } from 'src/app/services/monitor.service';
 
@@ -7,16 +8,21 @@ import { MonitorService } from 'src/app/services/monitor.service';
   templateUrl: './monitor-list.component.html',
   styleUrls: ['./monitor-list.component.css']
 })
-export class MonitorListComponent implements OnInit{
+export class MonitorListComponent implements OnInit, OnDestroy{
 
-  monitors:Monitor[] = []
+  monitors:Monitor[] = [];
+  monitorSubscription:Subscription= new Subscription();
   constructor(public monitorService:MonitorService) { }
 
   ngOnInit(): void {
     this.monitors = [{id:1,keywords:"3090"}]
     //this.monitorService.fetchMonitors();
-    this.monitorService.getMonitors().subscribe(monitors => {
+    this.monitorSubscription = this.monitorService.getMonitors().subscribe(monitors => {
       //this.monitors = monitors;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.monitorSubscription.unsubscribe();
   }
 }
