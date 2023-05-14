8fb50a87ef13b80a258278d3c441c6b9b1662710
Paolo Bartolucci <paolobarto210@gmail.com>
2023-05-13 20:59:27 -0400

    Monitor List and new monitor added

--- client/eSentry/src/app/components/home/home.component.ts
+++ client/eSentry/src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { SocialAuthService } from '@abacritt/angularx-social-login';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DEFAULT_USER } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -8,13 +9,37 @@ import { UserService } from 'src/app/services/user.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
-  user = this.userService.getCurrentUser();
+  user = DEFAULT_USER;
+  showDialog = false;
+
+  items = [
+    {
+      label:'New Monitor',
+      icon:'pi pi-plus',
+      command: () => this.showDialog = true
+    },
+    {
+      label:'Logout',
+      icon:'pi pi-power-off',
+      command: () => this.logout()
+    }
+  ]
 
   constructor(public router: Router,
     public socialAuthService: SocialAuthService,
     public userService: UserService) { }
+
+  ngOnInit(): void {
+    const currentUser = this.userService.getCurrentUser()
+    if (currentUser!=DEFAULT_USER) {
+      this.user = currentUser;
+    }
+    else {
+      this.router.navigate(['']);
+    }
+  }
 
   logout() {
     this.socialAuthService.signOut().then(() => this.router.navigate(['']));
