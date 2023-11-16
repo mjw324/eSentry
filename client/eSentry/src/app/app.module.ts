b7efe324bb3c64fec73cd3ac99fcec017a475687
Paolo Bartolucci <paolobarto210@gmail.com>
2023-11-16 13:12:04 -0500

    Added AuthGaurd, table ui, fetch

--- client/eSentry/src/app/app.module.ts
+++ client/eSentry/src/app/app.module.ts
@@ -10,8 +10,13 @@ import { TableModule } from 'primeng/table';
 import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ToastModule } from 'primeng/toast';
 
-import { GoogleLoginProvider, GoogleSigninButtonModule, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
+import {
+  GoogleLoginProvider,
+  GoogleSigninButtonModule,
+  SocialAuthServiceConfig,
+} from '@abacritt/angularx-social-login';
 
 import { MonitorListComponent } from './components/monitor-list/monitor-list.component';
 import { NewMonitorMenuComponent } from './components/new-monitor-menu/new-monitor-menu.component';
@@ -20,7 +25,7 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
-
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [
@@ -28,7 +33,7 @@ import { HttpClientModule } from '@angular/common/http';
     WelcomeComponent,
     HomeComponent,
     MonitorListComponent,
-    NewMonitorMenuComponent
+    NewMonitorMenuComponent,
   ],
   imports: [
     BrowserModule,
@@ -44,22 +49,28 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule,
     InputTextareaModule,
     InputTextModule,
+    ToastModule,
   ],
-  providers: [{
-    provide: 'SocialAuthServiceConfig',
-    useValue: {
-      autoLogin: true, //keeps the user signed in
-      providers: [
-        {
-          id: GoogleLoginProvider.PROVIDER_ID,
-          provider: new GoogleLoginProvider('868479157526-e4d3rgraqcudfi9rgqflimjb9qogv1mh.apps.googleusercontent.com') // your client id
-        }
-      ],
-      onError: (err: any) => {
-        console.error(err);
-      }
-    } as SocialAuthServiceConfig,
-  }],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: true, //keeps the user signed in
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '868479157526-e4d3rgraqcudfi9rgqflimjb9qogv1mh.apps.googleusercontent.com'
+            ), // your client id
+          },
+        ],
+        onError: (err: any) => {
+          console.error(err);
+        },
+      } as SocialAuthServiceConfig,
+    },
+    MessageService,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- client/eSentry/src/app/services/monitor.service.ts
+++ client/eSentry/src/app/services/monitor.service.ts
@@ -1,34 +1,66 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { Monitor } from '../models/monitor.model';
-import { HttpClient, HttpParams} from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { MonitorRequest } from '../models/monitor-request.model';
 import { environment } from 'src/environments/environment';
-
+import { MessageService } from 'primeng/api';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MonitorService {
-
   monitors: Subject<Monitor[]> = new Subject<Monitor[]>();
 
-  constructor(public http: HttpClient) { }
+  constructor(public http: HttpClient, public messageService: MessageService) {}
 
   getMonitors(): Observable<Monitor[]> {
     return this.monitors.asObservable();
   }
 
-  fetchMonitors(userid: string) {
-    const params = new HttpParams().set('userid', userid);
-    this.http.get<Monitor[]>(environment.url + '/monitors', { params }).subscribe(monitors => {
-      this.monitors.next(monitors);
-    });
+  async fetchMonitors(userid: string) {
+    // const params = new HttpParams().set('userid', userid);
+    // this.http
+    //   .get<any>(environment.url + '/monitors', { params })
+    //   .subscribe({
+    //     next: (monitors) => {
+    //       this.monitors.next(monitors);
+    //     },
+    //     error: (err) =>
+    //       this.messageService.add({
+    //         severity: 'error',
+    //         summary: 'Fetch Error',
+    //         detail: err.message,
+    //       }),
+    //     complete: () => {
+    //       console.info('Request Complete');
+    //     },
+    //   });
+
+
+      const data = await fetch(environment.url + '/monitors',{
+        method:"GET",
+        headers:{
+          'userid':userid
+        }
+      }).catch((error)=>{
+        this.messageService.add({severity:"error",summary:"Error",detail:error.message})
+        this.monitors.next([])
+        return
+      })
+
+      
+
+      data?.json().then((monitors)=>{
+        this.monitors.next(monitors)
+      })
   }
 
   addMonitor(request: MonitorRequest, userid: string) {
-    this.http.post<Monitor>(environment.url + '/monitors', request).subscribe(monitor => {
-      this.fetchMonitors(userid);
-    });
+    this.http
+      .post<Monitor>(environment.url + '/monitors', request)
+      .subscribe((monitor) => {
+        this.fetchMonitors(userid);
+      });
   }
 }
