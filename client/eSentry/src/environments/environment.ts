a181d9fc4dd0c4806db8104acfc126e4e3067eed
Micah Worth <mjw324@lehigh.edu>
2023-11-20 20:42:26 -0500

    Added option submit fields for min max price, exclude keywords, and conditions

--- client/eSentry/src/environments/environment.ts
+++ client/eSentry/src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    url: 'http://localhost:3000'
+    url: 'https://the-united-front.net',
+    google_client_id: '248593758771-0hkuh6k2n2d9u6qaqvsdibkfvnukmriq.apps.googleusercontent.com'
 };
